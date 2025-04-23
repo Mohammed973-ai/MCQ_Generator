2fb3f2f3e1f98b065e22c735ffc361c7407aeb82
Mohammed Waseem <115747766+Mohammed973-ai@users.noreply.github.com>
2025-04-23 18:00:46 +0000

    file updated

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -2,7 +2,7 @@ import os
 import json
 import traceback    
 import pandas as pd
-from dotenv import load_dotenv  
+# from dotenv import load_dotenv  
 from src.MCQ_generator.utils import read_file,get_table_data
 import streamlit as st  
 from src.MCQ_generator.MCQ_generator import generate_evaluate_chain 
